d5b6018b5bc673f12c854162c05f7253b4681128
Drin <drin@A3Lab.com>
2026-03-05 11:13:11 +0800

    完成作业1.2第一问代码，未作图

--- codes/probability.f90
+++ codes/probability.f90
@@ -1,45 +1,24 @@
 Program main
 implicit none
 real*4 y
-integer i,j,m,n
+integer i,m,n
 
 ! setting up the variables
-y = 1.0
-!n = 499
 m = 600
 
-do j=1,300
-    n = j
-    y = 1.0
-    ! reduce n to the smaller of n and m-n
-    if (n > m - n) n = m - n
 
-    ! different formula for even and odd n
-    if (mod(n,2) == 0) then
-        y = 1.1
-    else
-        y = y * (m - n + 1)/REAL(n)
-        n = n - 1
-    end if
-
- !   if (y < 1e-6) then 
- !       y = 0
-!    else
-        y = y*(2.**-(m - 2*n))
-!    end if
-    write (2,*) j,n,y,log10(y)
-
-
-    ! loop to calculate the probability
-    do i=1,n/2
-        y=y*(m + 1 - i)/4./(n  + 1 - i)
-        y=y*(m - n + i)/REAL(i)/4.
-        !write(1,*) i, y, log10(y)
+do n=1,300
+    y = - m * log(2.0)
+    do i=1,n
+        y = y + log(REAL(m - n + i)) - log(REAL(n + 1 - i))
     enddo
-
-    ! final calculation of the probability
-    write (1,*) j,n,y,log10(y)
-
+    if (y + 6 * log(10.0) < 0) then
+        y = 0
+    else
+        y = exp(y)
+    endif
+    
+    write(1,*) n,y,log(y)
 enddo
 
 end program main
